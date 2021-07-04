59c76f0e337c847be41f4f0ea6a705e54177f79f
Jonathan <38893484+Joonnaarr@users.noreply.github.com>
2021-07-04 00:36:42 -0300

    commit_4-7-21

---
+++ ProgramacionEstructurada/Clase_13/Ejercicio01/Ejercicio01.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Una consultora tiene 10 analistas y 15 proyectos en los que trabajan indistintamente, a fin de mes cada analista eleva una planilla con los siguientes datos
+
+
+o	número de analista 
+
+o	número de proyecto
+
+o	cantidad de hs trabajadas
+
+
+en memoria se debera cargar previamente el nombre de los analistas y el valor de la hora
+
+
+
+
+se desea saber:
+
+
+a.	cantidad de hs. trabajadas en total por cada analista.
+
+b.	total de horas trabajadas por cada analista en cada proyecto.
+
+c.	total de hs trabajadas sobre cada proyecto.
+
+d.	que analista trabajó menos en el proyecto 1.
+
+e.	sueldo de cada analista.
+
+f.	nombre del analista que cobró más.
+
+g.	cantidad de analistas que hayan trabajado menos de 5 hs en alguno de los proyectos.
+
+h.	imprimir ordenado en forma descendente por sueldo de los analista:
+
+
+-	nombre
+
+-	valor de la hora
+
+-	sueldo  */
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
